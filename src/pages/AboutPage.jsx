53b44f9abf22bd92b112c58eedaca4821c692ba4
alphury <aryansinghkaushik2004@gmail.com>
2026-03-16 23:18:03 +0530

    fixed error

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,6 +1,6 @@
 import Navbar from "../components/layout/Navbar";
 import "../styles/about.css";
-import raviImage from "../assets/images/raviimg.jpg";
+import raviImage from "../assets/images/raviimg.jpeg";
 
 function AboutPage() {
   return (
